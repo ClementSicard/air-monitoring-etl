625c328b8a0bbaaf869dce56dd39ca5facb15fbf
Clément Sicard <clement.sicard@icloud.com>
2025-03-29 17:45:40 +0100

    fix tests

--- src/air_monitoring/main_test.py
+++ src/air_monitoring/main_test.py
@@ -1,10 +1,26 @@
 """Tests for the main function."""
 
+from datetime import datetime
+from unittest import mock
 
-def test_1() -> None:
+from air_monitoring.data.measurement import Ecco2Measurement
+
+
+example_measurement = Ecco2Measurement(
+    timestamp=datetime.now(),
+    reference="12345-reference",
+    temperature=25.0,
+    humidity=50.0,
+)
+
+
+@mock.patch(
+    "air_monitoring.main.Ecco2.get_measurement",
+    return_value=example_measurement,
+)
+def test_get_measurement(get_measurement_patch) -> None:
     """Test that the main function does not raise an AssertionError."""
     from air_monitoring.main import main
 
     main()
-
     assert True
